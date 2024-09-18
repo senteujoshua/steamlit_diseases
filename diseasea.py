744ed06ade53a6ae2a4730962f7e128ce65045e9
Senteu <117025098+senteujoshua@users.noreply.github.com>
2024-09-19 01:00:12 +0300

    Add files via upload

---
+++ diseasea.py
@@ -0,0 +1,41 @@
+import numpy as np
+import pandas as pd
+import math
+
+from sklearn.linear_model import LinearRegression
+from sklearn import preprocessing,svm
+from sklearn.model_selection import train_test_split
+df = pd.read_csv('C:/Users/Admin/OneDrive/Documents/Python Scripts/Training.csv')
+# print(df.head())
+
+df['labels'] = df.iloc[:,-2]
+x = np.array(df.drop(columns=['prognosis', 'labels', 'Unnamed: 133'], axis=1))
+
+label_encoder = preprocessing.LabelEncoder()
+df['labels'] = label_encoder.fit_transform(df['labels'])
+# df['labels'].unique()
+y = np.array(df["labels"])
+# print(df['labels'].unique())
+x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.2)
+# print(x_test,y_test)
+classifier = svm.SVR()
+classifier.fit(x_train,y_train)
+Accuracy = classifier.score(x_test,y_test)
+# print(Accuracy)
+col_names = list(df.columns)
+# print(col_names)
+col_names = col_names[:-3]
+# print(col_names)
+patient_sympt = []
+
+for col in col_names:
+    sympt = input(f'Do you have {col} (yes/no): ')  # Asking for the symptom
+    if sympt.lower() == 'yes':
+        patient_sympt.append(1)  # 1 for "yes"
+    else:
+        patient_sympt.append(0)  # 0 for "no"
+patient_sympt = np.array(patient_sympt)
+patient_sympt = patient_sympt.reshape(1,-1)
+prediction = classifier.predict(patient_sympt)
+label = label_encoder.classes_[int(prediction)]
+print(f'You have {Accuracy*100:.2f}% chance of having {label}')
